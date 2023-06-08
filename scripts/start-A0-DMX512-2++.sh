a428f06759474de797b375789c6a384384328e1a
hohno-46466 <hohno.46466@gmail.com>
2023-06-08 22:45:49 +0900

    Updated by @hohno_at_kuimc on hohno-SP6-2019H

--- scripts/start-A0-DMX512-2++.sh
+++ scripts/start-A0-DMX512-2++.sh
@@ -15,7 +15,7 @@ MQTT_TOPIC2=hohno/DMX512
 
 set -x
 mosquitto_sub -t $MQTT_TOPIC1 \
-| awk '{printf "1c %dw\n201c %dw\n2c %dw\n202c %dw\n4c %dw\n206c %dw\n6c %dw\n203c %dw\n7c %dw\n203c %dw\n8c %dw\n203c %dw\n9c %dw\n203c %dw\n5c %dw\n205c %dw\n", $1,$1, $2,$2, $3,$3, $4,12, $5,32, $6,40, $7,4, $8,255; fflush()}' \
+| awk '{printf "1c %dw\n201c %dw\n2c %dw\n202c %dw\n4c %dw\n206c %dw\n6c %dw\n203c %dw\n7c %dw\n203c %dw\n8c %dw\n203c %dw\n9c %dw\n203c %dw\n5c %dw\n205c %dw\n", $1,$1, $2,$2, $3,$3, $4,12, $5,32, $6,40, $7,4, $8*0,255; fflush()}' \
 | awk '{if ($2 != xx[$1]) {xx[$1]=$2; printf " %s\n",$0; fflush()}}' \
 | mosquitto_pub -l -t $MQTT_TOPIC2
 
